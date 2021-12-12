7685fce825730d3c26325dc2b7636f5de9bbbead
MilosDrljaca <drljaca.milos1985@gmail.com>
2021-12-12 14:12:59 +0100

    Update Config.cs for IS4 OpenID Connect with MVC Interactive Client and TestUser

--- IdentityServer/Config.cs
+++ IdentityServer/Config.cs
@@ -1,8 +1,11 @@
-﻿using IdentityServer4.Models;
+﻿using IdentityModel;
+using IdentityServer4;
+using IdentityServer4.Models;
 using IdentityServer4.Test;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace IdentityServer
@@ -19,7 +22,30 @@ namespace IdentityServer
                         new Secret("secret".Sha256())
                     },
                     AllowedScopes = {"movieAPI"}
-                }
+                },
+
+                new Client{
+                    ClientId = "movies_mvc_client",
+                    ClientName = "Movies MVC Web App",
+                    AllowedGrantTypes = GrantTypes.Code,
+                    AllowRememberConsent = false,
+                    RedirectUris = new List<string>()
+                    { 
+                        "https://localhost:5002/signin-oidc"
+                    },
+                    PostLogoutRedirectUris = new List<string>()
+                    {
+                        "https://localhost:5002/signout-callback-oidc"
+                    },
+                    ClientSecrets = {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes = new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile
+                    }
+                },
             };
 
         public static IEnumerable<ApiScope> ApiScopes =>
@@ -36,11 +62,24 @@ namespace IdentityServer
         public static IEnumerable<IdentityResource> IdentityResources =>
             new IdentityResource[]
             {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile()
             };
 
         public static List<TestUser> TestUsers =>
             new List<TestUser>
             {
+                new TestUser
+                {
+                    SubjectId = "818727",
+                    Username = "Milos",
+                    Password = "Milos",
+                    Claims = new List<Claim>
+                    {
+                        new Claim(JwtClaimTypes.GivenName, "Milos"),
+                        new Claim(JwtClaimTypes.FamilyName, "Drljaca")
+                    }
+                }
             };
     }
 }
