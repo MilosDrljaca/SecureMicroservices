619190f1b0e1aa5d5bc8d0a89bb9ec68fc3b981d
MilosDrljaca <drljaca.milos1985@gmail.com>
2021-12-12 13:19:58 +0100

    Convert Movies.Client MVC Web Application to Consume Movies.API project

---
+++ Movies.Client/ApiServices/MovieApiService.cs
@@ -0,0 +1,51 @@
+﻿using Movies.Client.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Movies.Client.ApiServices
+{
+    public class MovieApiService : IMovieApiService
+    {
+        public async Task<IEnumerable<Movie>> GetMovies()
+        {
+            var movieList = new List<Movie>();
+
+            movieList.Add(
+
+                new Movie
+                {
+                    Id = 1,
+                    Genre = "Comics",
+                    Title = "Zagor",
+                    Rating = "9.0",
+                    ImageUrl = "images/src",
+                    ReleaseDate = DateTime.Now,
+                    Owner = "swn"
+                }
+                );
+            return await Task.FromResult(movieList);
+        }
+
+        public Task<Movie> GetMovie(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Movie> CreateMovie(Movie movie)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteMovie(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Movie> UpdateMovie(Movie movie)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
