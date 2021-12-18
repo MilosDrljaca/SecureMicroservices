fdd69027c3a39fb2e716edc01464a04bcca50bc2
MilosDrljaca <drljaca.milos1985@gmail.com>
2021-12-18 15:37:15 +0100

    Getting Token with OpenID Connect

--- Movies.Client/HttpHandlers/AuthenticationDelegatingHandler.cs
+++ Movies.Client/HttpHandlers/AuthenticationDelegatingHandler.cs
@@ -1,4 +1,7 @@
 ﻿using IdentityModel.Client;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,26 +13,39 @@ namespace Movies.Client.HttpHandlers
 {
     public class AuthenticationDelegatingHandler : DelegatingHandler
     {
-        private readonly IHttpClientFactory _httpClientFactory;
-        private readonly ClientCredentialsTokenRequest _tokenRequest;
+        private readonly IHttpContextAccessor _httpContextAccessor;
 
-        public AuthenticationDelegatingHandler(IHttpClientFactory httpClientFactory, ClientCredentialsTokenRequest tokenRequest)
+        public AuthenticationDelegatingHandler(IHttpContextAccessor httpContextAccessor)
         {
-            _httpClientFactory = httpClientFactory;
-            _tokenRequest = tokenRequest;
+            _httpContextAccessor = httpContextAccessor;
         }
+        //private readonly IHttpClientFactory _httpClientFactory;
+        //private readonly ClientCredentialsTokenRequest _tokenRequest;
+
+        //public AuthenticationDelegatingHandler(IHttpClientFactory httpClientFactory, ClientCredentialsTokenRequest tokenRequest)
+        //{
+        //    _httpClientFactory = httpClientFactory;
+        //    _tokenRequest = tokenRequest;
+        //}
 
         protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancelationToken)
         {
-            var httpClient = _httpClientFactory.CreateClient("IDPClient");
+            //var httpClient = _httpClientFactory.CreateClient("IDPClient");
 
-            var tokenResponse = await httpClient.RequestClientCredentialsTokenAsync(_tokenRequest);
-            if (tokenResponse.IsError)
+            //var tokenResponse = await httpClient.RequestClientCredentialsTokenAsync(_tokenRequest);
+            //if (tokenResponse.IsError)
+            //{
+            //    throw new HttpRequestException("Something went wrong while requesting the access token");
+            //}
+
+            //request.SetBearerToken(tokenResponse.AccessToken);
+
+            var accessToken = await _httpContextAccessor.HttpContext.GetTokenAsync(OpenIdConnectParameterNames.AccessToken);
+
+            if (!string.IsNullOrWhiteSpace(accessToken))
             {
-                throw new HttpRequestException("Something went wrong while requesting the access token");
+                request.SetBearerToken(accessToken);
             }
-
-            request.SetBearerToken(tokenResponse.AccessToken);
 
             return await base.SendAsync(request, cancelationToken);
         }
